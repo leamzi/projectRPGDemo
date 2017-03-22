66e768b136aa3b674cd3556be1114398239e03b9
Leamzi <ismael.serrada@gmail.com>
2017-03-22 15:03:23 -0300

    S01L6: A Simple DIY Follow Camera

---
+++ Dragon RPG/Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    
+	void LateUpdate () {
+
+        if (player == null)
+            return;
+
+        this.transform.position = player.transform.position;
+        
+    }
+}
